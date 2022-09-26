c676a837aa481462dc86f472c609b03ff8b961f7
xrousset78800 <xrousset@alchemy.fr>
2022-09-27 00:57:33 +0200

    clean code
    clean code

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <title>Scam</title>
   <meta name="description" content="Scam">
   <meta name="author" content="Scam">
-
+  
   <meta property="og:title" content="Scam">
   <meta property="og:type" content="website">
   <meta property="og:url" content="">
   <meta property="og:description" content="A simple Scam">
   <meta property="og:image" content="">
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="/scam-is-real/script.js"></script>
   <link rel="icon" type="image/png" href="/scam-is-real/favicon.png">
   <link href="/scam-is-real/style.css" rel="stylesheet">
+  
 </head>
   <body class="vsc-initialized">
   	<div class="status">
@@ -39,31 +40,25 @@
 				<h1 class="page-title"></h1>
 			</div>
 		</header>
-
         <main class="page-content">
-
-    <div class="twitch-video">
-		<!-- Load the Twitch embed JavaScript file -->
-		<script src="https://embed.twitch.tv/embed/v1.js"></script>
-
-		
-<script>
-$( document ).ready(function() {
-   loadScam();
-});
-</script>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9FP44RMCTK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-9FP44RMCTK');
-</script>
-    </div>
-
+			<div class="twitch-video">
+				<!-- Load the Twitch embed JavaScript file -->
+				<script src="https://embed.twitch.tv/embed/v1.js"></script>
+			</div>
       </main>
     </div>
-</body>
+	<script>
+	$( document ).ready(function() {
+	   loadScam();
+	});
+	</script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-9FP44RMCTK"></script>
+	<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-9FP44RMCTK');
+		</script>
+  </body>
 </html>
--- script.js
+++ script.js
@@ -4,65 +4,62 @@ let clientID = "vn9avm6d14fgwfyq0hc655klhwdcv8";
 var scamersTotalList = ['Xou____', 'Zed_B', 'Emizdar', 'patayencroute', 'amouranth', 'mistermv', 'zgolene', 'slackos_tv'];
 
 function loadScam() {
-var urlscammers = "";
+	var urlscammers = "";
 
-scamersTotalList.forEach(function(scam) {
-   urlscammers = urlscammers + "user_login=" + scam + "&";
-});
+	scamersTotalList.forEach(function(scam) {
+	   urlscammers = urlscammers + "user_login=" + scam + "&";
+	});
 
-var urlParams = new URLSearchParams(window.location.search);
-var scamGet = urlParams.getAll('scamer');
-var showChat = urlParams.get('active_chat');
+	var urlParams = new URLSearchParams(window.location.search);
+	var scamGet = urlParams.getAll('scamer');
+	var showChat = urlParams.get('active_chat');
 
-if(!showChat) {
-	showChat = "true";
-} 
+	if(!showChat) {
+		showChat = "true";
+	} 
 
-if(scamGet.length == 0) {
-	scamGet[0] = "Zed_B";
-} 
-if(scamGet.length == 1) {
-	jQuery(".page-title").append(scamGet[0] + " scam");
-} else {
-	jQuery(".page-title").append("Multi scam");
-}
+	if(scamGet.length == 0) {
+		scamGet[0] = "Zed_B";
+	} 
+	if(scamGet.length == 1) {
+		jQuery(".page-title").append(scamGet[0] + " scam");
+	} else {
+		jQuery(".page-title").append("Multi scam");
+	}
 
-StartThisShit(scamGet, showChat);
-jQuery("input[name=active_chat][value='"+showChat+"']").prop("checked",true);
+	StartThisShit(scamGet, showChat);
+	jQuery("input[name=active_chat][value='"+showChat+"']").prop("checked",true);
 
-  jQuery("h1.toggleShit").click(function(){
-    jQuery(".status").toggle(800,"linear");
-  });
+    jQuery("h1.toggleShit").click(function(){
+	    jQuery(".status").toggle(800,"linear");
+    });
 
-
-jQuery.ajax(
-{
-   type: 'GET',
-   url: 'https://api.twitch.tv/helix/streams?' + urlscammers,
-   headers: {
-     'Client-ID': clientID,
-     'Authorization': 'Bearer ' + authToken, 
-   },
-   success: function(c){
-      //data array is empty when queried channel is offline
-	if (c.data.length > 0) {
-		console.log(c.data);
-      }
-   	updateStatuses(c.data, scamGet);
-   }
-
-});
-
+	jQuery.ajax(
+	{
+	   type: 'GET',
+	   url: 'https://api.twitch.tv/helix/streams?' + urlscammers,
+	   headers: {
+		 'Client-ID': clientID,
+		 'Authorization': 'Bearer ' + authToken, 
+	   },
+	   success: function(c){
+		  //data array is empty when queried channel is offline
+		if (c.data.length > 0) {
+			console.log(c.data);
+		  }
+		updateStatuses(c.data, scamGet);
+	   }
+	});
 }
 
 function StartThisShit(scamers, showChat) {
 	jQuery("body").addClass("viewer"+scamers.length+"video");
-		var layout="video";
-		if(showChat == "true") {
-			layout="video-with-chat";
-		}
+	var layout="video";
+	if(showChat == "true") {
+		layout="video-with-chat";
+	}
 
-for(var i=0; i<scamers.length; i++){
+	for(var i=0; i<scamers.length; i++){
 		jQuery(".twitch-video").append("<div class='viewer'><div class='twitch-description' id='twitch-description"+(i+1)+"'></div><div class='twitch-embed'  id='twitch-embed"+(i+1)+"'></div></div>");
 		updateScammerOnlineBullshit(scamers[i], i+1);
 
@@ -72,23 +69,22 @@ for(var i=0; i<scamers.length; i++){
 			layout: layout,
 			channel: scamers[i]
 		  });	
-		 //jQuery(".twitch-video").append("<iframe id='twitch-chat-embed"+scamers[i]+"' src='https://www.twitch.tv/embed/"+scamers[i]+"/chat?parent=https://xouindaplace/scam-is-real/' height='100%' width='350'></iframe>");
- 
-}
+	//jQuery(".twitch-video").append("<iframe id='twitch-chat-embed"+scamers[i]+"' src='https://www.twitch.tv/embed/"+scamers[i]+"/chat?parent=https://xouindaplace/scam-is-real/' height='100%' width='350'></iframe>");
 
-		return true;
+	}
+	return true;
 }
 
 function updateStatuses(response, scamersList) {
 	jQuery(scamersTotalList).each(function(scam, value){
-            var scamer = value;
+        var scamer = value;
 		let flag = false;
 		
 		jQuery(response).each(function(value, user){
-		      if(user.user_login == scamer || user.user_name == scamer){
+		    if(user.user_login == scamer || user.user_name == scamer){
 				getBroadcast(user.user_id);
-                        updateScammerStatus(true, user); 
-			      flag = true;
+                updateScammerStatus(true, user); 
+			    flag = true;
 			}	
 		});
 		if(flag == false) {
@@ -97,7 +93,6 @@ function updateStatuses(response, scamersList) {
 		jQuery(scamersList).each(function(){
 			jQuery('.channel-form [value='+this+']').attr("checked", "checked");
 		});	
-		
 	});
 }
 
@@ -106,7 +101,9 @@ function updateScammerStatus(online, scamer) {
 		var duration = timeDiffCalc(new Date(scamer.started_at), new Date());
 		var thumbnail_resized = scamer.thumbnail_url.replace(/{width}|{height}/gi, 60);
 		var game = scamer.game_name;
-console.log(scamer);
+		
+		console.log(scamer);
+		
 		jQuery(".channel-form").prepend("<div class='channels'><input type='checkbox' id='"+scamer.user_name+"' name='scamer' value='"+scamer.user_name+"'/><div class='title'><img src='"+ thumbnail_resized+"'/>"+ scamer.title +"<div class='infos'>"+scamer.viewer_count+" Victimes</div><div class='date'>"+duration+"</div></div><i class='online-icon'></i><label for='"+scamer.user_name+"' >" + scamer.user_name + "</label><small class='game'><i>"+game+"</i></small></div>");
 	}else{
 		jQuery(".channel-form").prepend("<div class='channels'><input type='checkbox' id='"+scamer+"' name='scamer' value='"+scamer+"'/><i class='offline-icon'></i><label for='"+scamer+"'>" + scamer + "</label></div>");
@@ -119,26 +116,19 @@ function updateScammerOnlineBullshit(scamer,index) {
 
 function timeDiffCalc(dateFuture, dateNow) {
     let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
-
-    // calculate days
     const days = Math.floor(diffInMilliSeconds / 86400);
     diffInMilliSeconds -= days * 86400;
-
-    // calculate hours
     const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
     diffInMilliSeconds -= hours * 3600;
-
-    // calculate minutes
     const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
     diffInMilliSeconds -= minutes * 60;
-
     let difference = '';
+	
     if (days > 0) {
       difference += (days === 1) ? `${days}j` : `${days}j`;
     }
-
+	
     difference += (hours === 0 || hours === 1) ? `${hours}h` : `${hours}h`;
-
     difference += (minutes === 0 || hours === 1) ? `${minutes}m` : `${minutes}m`; 
 
     return difference;
@@ -165,9 +155,7 @@ jQuery.ajax(
 		console.log("bId erreur");
       }
    }
-
 });
-
 }
 
 
@@ -190,9 +178,7 @@ jQuery.ajax(
    error: function(c){
 
    }
-
 });
-
 }
 
 
@@ -219,7 +205,5 @@ jQuery.ajax(
         console.log("rewards NOK (rights)");
         console.log(c.responseJSON.message);
    }
-
 });
-
 }
