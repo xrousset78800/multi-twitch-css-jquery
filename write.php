c88089efd43ee1c1422f2453a3fa8de0224f2cf6
xrousset78800 <xrousset@alchemy.fr>
2022-10-13 23:29:20 +0200

    remove outdated files

--- write.php
+++
@@ -1,7 +0,0 @@
-<?php
-    error_reporting(E_ALL);
-    $data = $_POST['scam_price']; // the key we sent was "scam_price"
-    $f = fopen('scam.txt', 'w+');
-    fwrite($f, $data);
-    fclose($f);
-?>
